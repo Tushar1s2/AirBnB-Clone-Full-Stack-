aafe9ffe46b14ef4e4e1c915c95a10c882af4478
Tushar <tusharpanwar396@gmail.com>
2026-04-02 09:41:33 +0530

    Initialize passport package and tested with demo user

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const flash = require("connect-flash"); 
+const passport=require("passport");
+const localStrategy=require("passport-local");
+const User=require("./models/user.js");
 // Routes
 const listingsRoute=require("./routes/listing.js");
 const reviewRoute=require("./routes/review.js");
 const session=require("express-session");
+const { deserialize } = require("v8");
 const sessionOptions={
   secret:"mysupersecret",
   resave:false,
@@ -44,12 +48,29 @@ app.get("/", (req, res) => {
 });
 app.use(flash());
 
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // Middleware of flash
  app.use((req,res,next)=>{
   res.locals.success=req.flash("success");
   res.locals.error=req.flash("error");
   next();
  });
+
+app.get("/demouser",async(req,res) => {
+  let fakeUser=new User({
+    email:"abc@gmail.com",
+    username:"abc"
+  });
+
+  let registerUser=await User.register(fakeUser,"helloword");
+  res.send("registeredUser");
+})
 // Routes
 app.use("/listings",listingsRoute);
 app.use("/listings/:id/reviews",reviewRoute);
